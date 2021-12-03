2ac5d72bca7aec0249451513a417f92cba0ba09f
Joshibark <josiah.barkstrom@gmail.com>
2021-12-03 01:21:30 +0100

    Fixed NAvbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
 import ReceptVy from './ReceptVy';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
         <ReceptVy></ReceptVy>
-        
+        <Navbar></Navbar>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import HomeIcon from '@mui/icons-material/Home';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+export default function LabelBottomNavigation() {
+  const [value, setValue] = React.useState('recents');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <BottomNavigation sx={{ width: 360, position: 'absolute', left: '50%', top: '96.5%', transform: 'translate(-50%, -50%)'}} value={value} onChange={handleChange}>
+      <BottomNavigationAction
+        label="Inställningar"
+        value="Settings"
+        icon={<SettingsIcon />}
+      />
+
+      <BottomNavigationAction
+        label="Hem"
+        value="Home"
+        icon={<HomeIcon />}
+      />
+      
+      <BottomNavigationAction
+        label="Favoriter"
+        value="Favorite"
+        icon={<FavoriteIcon />}
+      />
+
+    </BottomNavigation>
+  );
+}

ef728598990e7342ce636d4145f0ae5e511a5999
Joshibark <josiah.barkstrom@gmail.com>
2021-12-08 06:53:54 +0100

    Styled favorites

--- src/Favorit.js
+++ src/Favorit.js
@@ -3,31 +3,30 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { Stack ,Chip, ListItem } from "@mui/material";
-
+import { Link } from "react-router-dom";
 
 export default function Favorit({nr}) {
-    console.log(nr)
     const title = window.localStorage.getItem('FavTitle'+nr)
     const port = window.localStorage.getItem('FavPort'+nr)
     const time = window.localStorage.getItem('FavTime'+nr)
     const ingr = JSON.parse(window.localStorage.getItem('FavIngredients'+nr))
   return(
     <div>
-    <Card sx={{ maxWidth: 375, backgroundColor: '#222165', color: 'white'}}>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {title}
-        </Typography>
-        <Stack align="center" direction="row" spacing={1}>
-          <Chip size="" label={"Portioner: " + port} />
-          <Chip label={time} variant="outlined" /> 
-        </Stack>
-        <Typography variant="body2">
-          <h2>Ingredienser</h2>
-          {ingr.map((element, index) => (<ListItem divider="true" key={index}>{element}</ListItem>))}
-        </Typography>
-      </CardContent>
-    </Card>
+        <Link style={{ textDecoration: 'none'}} to="/recept-roulette/Recept/">
+            <Card sx={{ alignContent: 'center', marginTop: 1, marginLeft: 1, width: 355, backgroundColor: '#F8CE7A', color: 'white'}}>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">{title}</Typography>
+                    <Stack align="center" direction="row" spacing={1}>
+                        <Chip size="" label={"Portioner: " + port} />
+                        <Chip label={time} variant="outlined" /> 
+                    </Stack>
+                    <Typography variant="body2">
+                        <Typography gutterBottom variant="h4">Ingredienser</Typography>
+                            {ingr.map((element, index) => (<ListItem divider="true" key={index}>{element}</ListItem>))}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Link>
     </div>
   );
 }
--- src/Favoriter.js
+++ src/Favoriter.js
@@ -3,15 +3,14 @@ import Favorit from './Favorit';
 
 export default function MediaCard() {
   let nr = JSON.parse(window.localStorage.getItem('nr'));
-  console.log(nr)
   const est = Array.from(Array(nr).keys())
   var tse = est.map(function(item){
     return item+1;
- 
  })
-  console.log(tse);
-
   return (
-    tse.map((i, index) => (<Favorit nr={i} {...console.log(i)} key={index}/>))
+    <div>
+      {tse.map((i, index) => (<Favorit nr={i} key={index}/>))}
+      <div className="spaceBottom"/>
+    </div>
   );
 }

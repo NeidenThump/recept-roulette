a4b848285e460b5605627b808cc02a71b197deda
Joshibark <josiah.barkstrom@gmail.com>
2021-12-09 19:08:05 +0100

    remove funkar helt nu

--- src/Favoriter.js
+++ src/Favoriter.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import Favorit from './Favorit';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function MediaCard() {
   let favoriter = JSON.parse(window.localStorage.getItem('favoriter'));
   return (
     <div>
-      {favoriter.map((i, index) => (<Favorit recipe={JSON.parse(i)} nr={index} key={index}/>))}
+      {favoriter.map((i, index) => (<Favorit recipe={JSON.parse(i)} nr={index} key={uuidv4()}/>))}
       <div className="spaceBottom"/>
     </div>
   );

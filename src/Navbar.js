0aaadd536cbb3a195fbb4226510055330aa13907
Elias <eliel224@student.liu.se>
2021-12-11 15:14:38 +0100

    Fixade navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,22 +3,22 @@ import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import HomeIcon from '@mui/icons-material/Home';
+import ArticleIcon from '@mui/icons-material/Article';
 import SettingsIcon from '@mui/icons-material/Settings';
 import {Link} from "react-router-dom";
 
 export default function LabelBottomNavigation() {
   const [value, setValue] = React.useState('recents');
 
-  const handleChange = (newValue) => {
+  const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <BottomNavigation sx={{ display: 'flex', justifyContent: 'space-around', width: 375, position: 'fixed', left: '50%', top: '96.5%', transform: 'translate(-50%, -50%)'}} value={value} onChange={handleChange}>
-      <Link to="/recept-roulette/Settings/"><BottomNavigationAction sx={{width: 125}} label="Inställningar" value="Settings" icon={<SettingsIcon />}/></Link>
-      <Link to="/recept-roulette/Recept/"><BottomNavigationAction sx={{width: 125}} label="Hem" value="Home" icon={<HomeIcon />}/></Link>
-      <Link to="/recept-roulette/Favoriter/"><BottomNavigationAction sx={{width: 125}} label="Favoriter" value="Favorite" icon={<FavoriteIcon />}/></Link>
+    <BottomNavigation sx={{ display: 'flex', justifyContent: 'space-around', width: 375, position: 'fixed', left: '50%', top: '97.2%', transform: 'translate(-50%, -50%)'}} value={value} onChange={handleChange}>
+      <BottomNavigationAction component={Link} to="/recept-roulette/Settings/" sx={{width: 125}} label="Inställningar" value="Settings" icon={<SettingsIcon />}/>
+      <BottomNavigationAction component={Link} to="/recept-roulette/Recept/" sx={{width: 125}} label="Recept" value="Home" icon={<ArticleIcon />}/>
+      <BottomNavigationAction component={Link} to="/recept-roulette/Favoriter/" sx={{width: 125}} label="Favoriter" value="Favorite" icon={<FavoriteIcon />}/>
     </BottomNavigation>
   );
 }
